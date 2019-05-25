3de7bfd826dc777511d5737914a8ae40c9e44bd2
LENOVO <thuyhpth1805004@fpt.edu.vn>
2019-05-25 19:36:53 +0700

    Initial commit

---
+++ PRACTICAL/Book.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICAL
+{
+    class Book : Product
+    {
+        public Book(int id, string name, double price, string producer) : base(id, name, price, producer)
+        {
+        }
+
+        public override double computeTax()
+        {
+            return this.price * 0.08;
+        }
+
+    }
+}
---
+++ PRACTICAL/Product.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICAL
+{
+
+    public abstract class Product
+    {
+        public  int id;
+        public  string name;
+        public double price;
+        public string producer;
+
+        protected Product()
+        {
+        }
+
+        public Product(int id, string name, double price, string producer)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.producer = producer;
+        }
+        public abstract double computeTax();
+
+    }
+}
